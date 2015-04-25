f523b2d89b1a3447ca0722598c10ec965332746a
Ruben <github-com-00ff86@vandamme.email>
2015-04-25 23:52:16 +0200

    Remove unused files.

--- build.sh
+++
@@ -1,1 +0,0 @@
-docker build -t matttbe/fdroid-server .
--- docker_run_fdroid.sh
+++
@@ -1,3 +0,0 @@
-export APK_REPO=~/apk
-mkdir -p $APK_REPO
-docker run --rm  -v $APK_REPO:/apk/repo -v $(pwd)/my_config.py:/apk/config.py matttbe/fdroid-server
